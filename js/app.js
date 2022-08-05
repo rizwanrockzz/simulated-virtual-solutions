d035537d16d6a70f7100bae1470e4d401a3ef448
rizwan <rizwan@hp.local>
2022-08-05 13:14:07 +0530

    upload files of simulatedvirtualsolutions.com

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Navbar
+
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0%";
+}
+
+// accordation
+
+var acc = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
